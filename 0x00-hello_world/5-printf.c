8381cc1d262c58a46d457b1ccf1e1f361f0516bd
AbebeGetnet <aabizy@gmail.com>
2022-12-08 13:59:24 -0800

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <studiio.h>
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
