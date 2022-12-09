80a08bcebc92edc025b58784fe60ff593231001b
AbebeGetnet <aabizy@gmail.com>
2022-12-08 16:42:31 -0800

    6&8

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-/**
- * Main: entry point
- * return : alawys 0
+
+/*
+ *
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -11,5 +15,4 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("size of float: %lu byte(s)\n", sizeof(float));
 	return (0);
-	
 }
