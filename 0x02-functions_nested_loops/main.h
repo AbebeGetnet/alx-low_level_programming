e657d96b9505d874af6bcd1e2328a91c0b2ff8ed
AbebeGetnet <aabizy@gmail.com>
2022-12-14 06:03:22 -0800

    printv the cstring _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int islower(int c);
+int isalpha(int c);
+int print_sign(int n);
+int _ans(int);
+int print_last_digit(int);
+int jack_bauer(void);
+void times_table(void);
+int add (int, int);
+void print_to_98(int n);
+int _putchar(char c);
+
+#endif
