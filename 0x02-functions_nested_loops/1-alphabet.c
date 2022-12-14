9a56083032d2db55e5f81c2253307a811ca605b5
AbebeGetnet <aabizy@gmail.com>
2022-12-14 09:32:09 -0800

    alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,13 @@
 #include "main.h"
-
 /**
  *
  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
 void print_alphabet(void)
 {
-	char letter;
-	
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+	char la;
 
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
 	_putchar('\n');
 }
