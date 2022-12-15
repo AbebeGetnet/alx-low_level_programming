838bb6414f2c6ea4a0969669ac3e03c9ce8cde45
AbebeGetnet <aabizy@gmail.com>
2022-12-14 23:13:31 -0800

    debuging project task two

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,9 @@
 #include "main.h"
+
 /**
 *main - Test function for positive or negative
 *return:0
 */
-
 int main(void)
 {
 	int i;
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,9 +3,9 @@
 
 #include <stdio.h>                                                                   
 void positive_or_negative(int i);                                 
-int largest_number(int a, in b, int c);                           
+int largest_number(int a, int b, int c);                           
 void print_remaining_days(int month, int days, int year);         
 int convert_day(int month, int day);                              
-void print_times_table(in n);
+void print_times_table(int n);
 
 #endif
