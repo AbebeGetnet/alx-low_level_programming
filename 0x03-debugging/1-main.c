ed9d06fb29ef1fb56dd7b1162ebe9bba1aad7e57
AbebeGetnet <aabizy@gmail.com>
2022-12-15 01:06:48 -0800

    task 2

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - cause an infinite loop
+* Return: 0
+*/
+int main(void)
+{
+int i;
+
+printf("infinite loop incomming :(\n");
+
+i = 0;
+/*
+*while(i < 10)
+*{
+*	putchar(i);
+*}
+*/
+printf("infinite loop avoid \\o/\n");
+
+return (0);
+}
