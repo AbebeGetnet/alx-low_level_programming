a3bfcba49e0a656547bf5a0252f9f72406ef9b16
AbebeGetnet <aabizy@gmail.com>
2022-12-14 16:56:28 -0800

    error correction

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,12 @@
 /*
-
  * File: 2-print_alphabet_x10.c
-
  * Auth: Brennan D Baraban
-
  */
-
-
 
 #inlude "main.h"
 
-
-
 /**
-
  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
-
  */
 void print_alphabet_x10(void)
 {
