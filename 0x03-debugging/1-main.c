e99baa3913c6258d04ae650e16aa07602f4380bc
AbebeGetnet <aabizy@gmail.com>
2022-12-15 18:02:51 -0800

    error solving for print remaining days

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 
 /**
-* main - cause an infinite loop
+* main - causes an infinite loop
 * Return: 0
 */
 int main(void)
 {
-int i;
+	int i;	
+	printf("Infinite loop incoming :(\n");
+	i = 0;
+	/* while (i < 10)
+	 *{
+	 *	putchar(i);
+	 *}
+	 */
+	printf("Infinite loop avoided! \\o/\n");
 
-printf("infinite loop incomming :(\n");
-
-i = 0;
-/*
-*while(i < 10)
-*{
-*	putchar(i);
-*}
-*/
-printf("infinite loop avoid \\o/\n");
-
-return (0);
+	return (0);
 }
--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,33 +1,36 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
  * print_remaining_days - takes a date and prints how many days are
- * left in the year. taking leap year in account
+ * left in the year, taking leap years into account
  * @month: month in number format
- * @days: days of month
+ * @day: day of month
  * @year: year
  * Return: void
  */
-void print_remaining_days(int month, int days, int year);
+
+void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+	if ((year % 400 == 0 && year % 100 == 0) || year % 4 == 0)
 	{
-		if (month > 2 && days == 60)
+		if (month >= 3 && day >= 60)
 		{
 			day++;
 		}
-		printf("Days of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - days);
+
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
 		if (month == 2 && day == 60)
 		{
-			printf("Invalid Date: %02d/%02d/%04d\n", month, day - 30, year);
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
-			printf("Day ofthe year: %d\n", day);
+			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
