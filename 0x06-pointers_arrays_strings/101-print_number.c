e192a15a9bc6618b8e0cbd63621922c790bc0c12
AbebeGetnet <aabizy@gmail.com>
2022-12-22 01:01:18 -0800

    rewrite print_number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,29 +1,22 @@
 #include "main.h"
-
-#define ROT13IN  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-#define ROT13OUT "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
-
 /**
- * print_number - using only _putchar
- * @n: the integer to print
+ * print_number - prints an integer
+ * @n:.input integer parameter
  *
- * Return: void
+ *
  */
 void print_number(int n)
 {
-	int i, p;
+	unsigned int i = n;
 
 	if (n < 0)
-		_putchar('-');
-	for (p = 0, i = 1000000000; i > 0; i /= 10)
 	{
-		int d = (n / i) % 10;
-
-		d = d < 0 ? -d : d;
-		if (d || p || i == 1)
-		{
-			_putchar(d + '0');
-			p++;
-		}
+		_putchar(45);
+		i = -i;
 	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
 }
