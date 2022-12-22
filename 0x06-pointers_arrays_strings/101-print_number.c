53b4ac05ca0115825e1c7277c9d79e92d83084a0
AbebeGetnet <aabizy@gmail.com>
2022-12-21 22:00:10 -0800

    error solving on 1001-print _number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,21 +1,29 @@
 #include "main.h"
 
+#define ROT13IN  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+#define ROT13OUT "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
+
 /**
- * print_number - function with one argument
- * @n: int type argument
- * Description: prints an integer
- * Return: value of n
+ * print_number - using only _putchar
+ * @n: the integer to print
+ *
+ * Return: void
  */
 void print_number(int n)
 {
+	int i, p;
+
 	if (n < 0)
-	{
-		n = -n;
 		_putchar('-');
-	}
-	if (n / 10)
+	for (p = 0, i = 1000000000; i > 0; i /= 10)
 	{
-		print_number(n / 10);
+		int d = (n / i) % 10;
+
+		d = d < 0 ? -d : d;
+		if (d || p || i == 1)
+		{
+			_putchar(d + '0');
+			p++;
+		}
 	}
-	_putchar(n % 10 + '0');
 }
