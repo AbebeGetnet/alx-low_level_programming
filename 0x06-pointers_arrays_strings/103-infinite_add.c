9dc569ee4aac9c116be47b656bc860c6a25478e0
AbebeGetnet <aabizy@gmail.com>
2022-12-22 00:19:10 -0800

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,33 +1,68 @@
-#include "main.h"
+#include "mainn.h"
 
 /**
- * infinite_add - function with 4 arguments
- * @n1: char type pointer argument
- * @n2: char type pointer argument
- * @r: char type pointer argument
- * @size_r: int type argument
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
  *
- * Description: adds two numbers from string
- * Return: sum of two integers
+ * Return: integer length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * rev_string - reverses a string
+ * @s: the string to reverse
+ *
+ * Return: void
+ */
+char *rev_string(char *s)
+{
+	int l = _strlen(s), i = 0;
+	char t;
+
+	for (i = 0; i < l / 2; i++)
+	{
+		t = s[l - i - 1];
+		s[l - i - 1] = s[i];
+		s[i] = t;
+	}
+	return (s);
+}
+
+/**
+ * infinite_add - adds arbitrarily long string of digits
+ * @n1: the first digit string
+ * @n2: the second digit string
+ * @r: the result buffer
+ * @size_r: the size of result buffer
+ *
+ * Return: char pointer to buffer
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int count, count2;
+	int l1 = _strlen(n1), l2 = _strlen(n2), i = 0, a, b, c = 0;
 
-	while (n1[count] != '\0')
-		count++;
-	while (n2[count2] != '\0')
-		count2++;
-
-	*r = *(r + size_r);
-	while (n1[count] > 0 || n1[count2] > 0)
+	for (l1--, l2--, size_r--; l1 >= 0 || l2 >= 0 || c; l1--, l2--, i++)
 	{
-		if (n1[count] + n2[count2] > 0)
-			*r = n1[count - 1] + n2[count2 - 1] + 1;
-		else
-			*r = n1[count] + n2[count2];
-		count--;
-		count2--;
+		if (i >= size_r)
+			return (0);
+		a = 0;
+		b = 0;
+		if (l1 >= 0)
+			a = n1[l1] - '0';
+		if (l2 >= 0)
+			b = n2[l2] - '0';
+		a = a + b + c;
+		c = a / 10;
+		a %= 10;
+		r[i] = a + '0';
 	}
-	return (r);
+	r[i] = '\0';
+	return (rev_string(r));
 }
