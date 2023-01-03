f091b32d9b6e60d62308336de8339d2e72fbd5d4
AbebeGetnet <aabizy@gmail.com>
2023-01-03 04:04:33 -0800

    errror solving for task 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,7 +3,7 @@
 /**
  * print_diagsums - print additions of the two diagonals.
  * @a: source matrix
- * @size: size NxN of matrix
+ * @size: size NxN of matri
  *
  * Return: chessboard.
  */
@@ -11,10 +11,8 @@ void print_diagsums(int *a, int size)
 {
 	int i, j, sumdiag1 = 0, sumdiag2 = 0, step1, step2;
 
-	for (i = 0; i <= (size - 1); i+)
-
+	for (i = 0; i <= (size - 1); i++)
 	{
-
 		step1 =  (size + 1) * i;
 		sumdiag1 = sumdiag1 + *(a + step1);
 	}
