f79fef890fa7c17ba9817caab4fe77d345c4facf
AbebeGetnet <aabizy@gmail.com>
2023-01-10 04:19:06 -0800

    update main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,12 +1,7 @@
 #ifndef MAIN_H
-#define MAIN_H
-/*
- * File: holberton.
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0A-malloc_free directory
- */
 
+#define MAIN_H
+int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
