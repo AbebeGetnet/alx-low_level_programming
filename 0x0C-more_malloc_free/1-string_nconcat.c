a32d1f0ee03fb0a9279758468aaa60f1b02480cf
AbebeGetnet <aabizy@gmail.com>
2023-01-12 04:35:57 -0800

    edit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,33 +1,28 @@
-#include "main.h"
+#include <stdlib.h>
 /**
- * string_nconcat - concatenates 2 strings
+ * string_nconcat - concatenate `s1' and the first `n' bytes of `s2'
  * @s1: first string
  * @s2: second string
- * @n: number of bytes to copy from second string
- * Return: pointer to new string
+ * @n: number of bytes from `s2'
+ * Return: pointer to concatenated strings, or NULL on failure
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, len1 = 0, len2 = 0;
-	char *new_str;
+	unsigned int i, j;
+	char *p;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	while (*(s1 + len1) != '\0')
-		len1++;
-	while (*(s2 + len2) != '\0')
-		len2++;
-	if (n >= len2)
-		n = len2;
-	new_str = malloc(sizeof(char) * (len1 + n + 1));
-	if (new_str == NULL)
+	for (i = 0; s1 && s1[i]; ++i)
+		;
+	for (j = 0; s2 && s2[j]; ++j)
+		;
+	n = (n > j ? j : n);
+	p = malloc(sizeof(char) * (i + n + 1));
+	if (p == NULL)
 		return (NULL);
-	for (i = 0; i < len1; i++)
-		new_str[i] = s1[i];
-	for (j = 0; j < n; i++, j++)
-		new_str[i] = s2[j];
-	new_str[i] = '\0';
-	return (new_str);
+	for (i = 0; s1 && s1[i]; ++i)
+		p[i] = s1[i];
+	for (j = 0; s2 && j < n; ++j)
+		p[i + j] = s2[j];
+	p[i + j] = '\0';
+	return (p);
 }
