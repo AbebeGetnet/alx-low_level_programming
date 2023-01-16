30295a875603597487f25c84b6343e2d9638bc70
AbebeGetnet <aabizy@gmail.com>
2023-01-16 04:39:07 -0800

    edit task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,91 +1,64 @@
-#include <stdlib.h>
 #include "dog.h"
-
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
-  * new_dog - ...
-  * @name: ...
-  * @age: ...
-  * @owner: ...
-  *
-  * Return: ...
+ * _strdup - malloc new str and copy *str into it
+ * @str: the string we copy
+ * Return: pointer to new string
+ */
+char *_strdup(char *str)
+{
+	char *dest;
+	unsigned int i;
+
+	int count = 0;
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+	{
+		count++;
+	}
+	count += 1;
+	dest = malloc(count * sizeof(char));
+	if (dest == NULL)
+
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		dest[i] = str[i];
+	}
+	dest[i] = str[i];
+	return (dest);
+}
+/**
+  * new_dog - creates a new dog
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: owner
+  * Return: pointer to new dog
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *cutie_dog;
-	int name_l = 0, own_l = 0;
+	dog_t *puppy_dog;
 
-	if (name != NULL && owner != NULL)
+	puppy_dog = malloc(sizeof(dog_t));
+	if (puppy_dog == NULL)
+		return (NULL);
+	puppy_dog->name = _strdup(name);
+	if (puppy_dog->name == NULL)
 	{
-		name_l = _strlen(name) + 1;
-		own_l = _strlen(owner) + 1;
-		cutie_dog = malloc(sizeof(dog_t));
-
-		if (cutie_dog == NULL)
-			return (NULL);
-
-		cutie_dog->name = malloc(sizeof(char) * name_l);
-
-		if (cutie_dog->name == NULL)
-		{
-			free(cutie_dog);
-			return (NULL);
-		}
-
-		cutie_dog->owner = malloc(sizeof(char) * own_l);
-
-		if (cutie_dog->owner == NULL)
-		{
-			free(cutie_dog->name);
-			free(cutie_dog);
-			return (NULL);
-		}
-
-		cutie_dog->name = _strcpy(cutie_dog->name, name);
-		cutie_dog->owner = _strcpy(cutie_dog->owner, owner);
-		cutie_dog->age = age;
-	
-
-	return (cutie_dog);
-}
-
-/**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(char *s)
-{
-	int c = 0;
-
-	for (; *s != '\0'; s++)
-	{
-		c++;
+		free(puppy_dog);
+		return (NULL);
 	}
-
-	return (c);
-}
-
-/**
-  * _strcpy - Copy a string
-  * @dest: Destination value
-  * @src: Source value
-  
-  * Return: the pointer to dest
-  */
-char *_strcpy(char *dest, char *src)
-{
-	int i;
-
-	for (i = 0; src[i] != '\0'; i++)
+	puppy_dog->owner = _strdup(owner);
+	if (puppy_dog->owner == NULL)
 	{
-		dest[i] = src[i];
+		free(puppy_dog->name);
+		free(puppy_dog);
+		return (NULL);
 	}
-
-	dest[i++] = '\0';
-
-	return (dest);
+	puppy_dog->age = age;
+	return (puppy_dog);
 }
