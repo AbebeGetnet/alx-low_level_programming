5f46a50484b33b852c8daa71900460ab88b4cd53
AbebeGetnet <aabizy@gmail.com>
2023-01-20 02:05:57 -0800

    add some files

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,47 +1,35 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 /**
- * print_opcodes - program that prints the opcodes of its own main function.
- * @a: address of the main function
- * @n: number of bytes to print
- * Return: nothing.
+ * main - program that prints the opcodes of its own main function.
+ * @argc: counter
+ * @argv: vector
+ * Return: Always 0 (Success)
  */
-void print_opcodes(char *a, int n)
+int main(int argc, char **argv)
 {
 	int i;
-
-	for (i = 0; i < n; i++)
-	{
-		printf("%.2hhx", a[i]);
-		if (i < n - 1)
-		{
-			printf(" ");
-		}
-	}
-	printf("\n");
-}
-/**
- * main - prints the opcodes of its own main function
- * @argc: integer
- * @argv: character
- * Return: always 0.
- */
-int main(int argc, char *argv[])
-{
-	int ae;
+	int bytes;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	ae = atoi(argv[1]);
-	if (ae < 0)
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	print_opcodes((char *)&main, ae);
-
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02x", ((unsigned char *)main)[i]);
+		if (i != (bytes - 1))
+			printf(" ");
+		else
+			printf("\n");
+	}
 	return (0);
 }
--- 0x0F-function_pointers/2-main.c
+++ 0x0F-function_pointers/2-main.c
@@ -24,7 +24,7 @@ int is_strictly_positive(int elem)
 }
 
 
-/**
+/*
  * abs_is_98 - check if the absolute value of a number is 98
  * @elem: the integer to check
  *
@@ -49,7 +49,7 @@ int main(void)
 	printf("%d\n", index);
 	index = int_index(array, 20, abs_is_98);
 	printf("%d\n", index);
-	index = int_index(array, 20, is_strictly_positive);
+	index = int_index(array, 20, is_strictly_positive)
 	printf("%d\n", index);
 	return (0);
 }
