a30b789023020cfe47c5e62bbcee45904649762f
AbebeGetnet <aabizy@gmail.com>
2023-01-20 01:53:11 -0800

    eddit some tasks

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,52 +1,45 @@
-#include <stdio.h>
-#include "function_pointers.h"
-#include <stdlib.h>
-#include <string.h>
+#include "3-calc.h"
 
 /**
- * main - check the program
- * @argc: arguments cant
- * @argv: vector arguments
- * Return: 0 if false, something else otherwise.
+ * main - entry point, screens input and uses function pointer to
+ * choose operator on a and b
+ *
+ * @argc: amount of command line arguments
+ *
+ * @argv: array of strings containing arguments
+ *
+ * Return: 0 upon success
  */
+
 int main(int argc, char *argv[])
 {
-	int (*get)(int, int);
-	int a, b, operator;
+	int a;
+	int b;
+	int result;
 
 	if (argc != 4)
 	{
-		printf("rror\n");
-
+		printf("Error\n");
 		exit(98);
 	}
-	if (strlen(argv[2]) != 1)
+
+	if (get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	operator = argv[2][0];
-	witch (operator)
 
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
 	{
-	case '+':
-		break;
-	case '-':
-		break;
-	case '*':
-		break;
-	case '/':
-		break;
-	case '%':
-		break;
-	default:
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	get = get_op_func(argv[2]);
-	printf("%d\n", get(a, b));
+
+	result = get_op_func(argv[2])(a, b);
+	printf("%d\n", result);
+
 	return (0);
 }
