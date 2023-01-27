0776bc0072c2bcf511aa8b9a07661949cdf7cf0c
AbebeGetnet <aabizy@gmail.com>
2023-01-26 16:55:56 -0800

    101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,17 @@
-ection .text
-   global main
-
-main:
-   push rbp
-
-   mov rdi,fmt
-   mov rsi,msg
-   mov rax,0
-   call printf
-
-   pop rbp
-
-   mov rax,0
-   ret
-
+extern print
 section .data
-   msg: db "Hello, AG", 0
-   fmt: db "%s", 10, 0
+	msg: db "Hello, Holberton", 0
+	fmt: db "%s", 10, 0
+
+section .text
+	global main
+main:
+	push rbp
+	mov rdi,fmt
+	mov rsi,msg
+	mov rax,0
+	call printf
+
+	pop rbp
+	mov rax,0
+	ret
