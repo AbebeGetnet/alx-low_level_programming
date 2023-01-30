d6a8ea21c17c084258fdbcd92f674992596eee6c
AbebeGetnet <aabizy@gmail.com>
2023-01-30 10:30:49 -0800

    updatectask 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,9 +13,9 @@ size_t print_listint(const listint_t *h)
 
 	while (h)
 	{
+		nodes++;
 		printf("%d\n", h->n);
-		h = h->next;
-                nodes++;                                                                                                
+		h = h->next;                                                                                                
 	}
 
 	return (nodes);
