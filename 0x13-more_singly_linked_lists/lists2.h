b05b931160cd2bb19fac8cb8e3a054d956d1b613
AbebeGetnet <aabizy@gmail.com>
2023-01-30 09:40:25 -0800

    update advanced tasks

--- 0x13-more_singly_linked_lists/lists2.h
+++ 0x13-more_singly_linked_lists/lists2.h
@@ -1,19 +1,39 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-/*
- * File: lists.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes and definitions for all functions
- *       and types written in the 0x12-more_singly_linked_lists directory.
- */
+#ifndef STDLIB_H
+#define STDLIB_H
 #include <stdlib.h>
+#endif
+
+#ifndef _LISTS_H
+#define _LISTS_H
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+/* 0x12. C - More singly linked lists */
 
 /**
  * struct listint_s - singly linked list
  * @n: integer
-  @next: points to the next node
-
+ * @next: points to the next node
  *
  * Description: singly linked list node structure
  * for Holberton project
@@ -23,6 +43,8 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
+
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -37,7 +59,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t **h);
-listint_t *find_listint_loop(listint_t *head);
 
-#endif /* LISTS_H */
+
+#endif
+
+
+
